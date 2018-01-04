943d05ba8e5c84583fc63ed34f433592555cfd29
Shantanu <shantanu2846@gmail.com>
2018-01-04 13:32:28 +0530

    Add enqueue_editor_assets method to the core plugin class

--- includes/class-conditional-blocks.php
+++ includes/class-conditional-blocks.php
@@ -40,7 +40,26 @@ if ( !class_exists( 'Conditional_Blocks' ) ) {
 			
 			$this->enqueue_block_assets();
 		}
+		
+		
+		/**
+		 * Enqueue Editor Assets
+		 * 
+		 * Enqueue the assets on the editor side.
+		 * 
+		 * `wp-blocks`: includes block type registration and related functions.
+		 * `wp-element`: includes the WordPress Element abstraction for describing the structure of your blocks.
+		 * `wp-i18n`: To internationalize the block's text.
+		 * 
+		 * @since 0.0.1
+		 */
+		public function enqueue_editor_assets() {
 
-	}
+			wp_enqueue_script( 'conditional_block', CB_URL . 'assets/js/block.js', array( 'wp-blocks', 'wp-i18n', 'wp-element' ) );
+			
+			wp_enqueue_style( 'conditional_block_editor', CB_URL . 'assets/css/editor.css', array( 'wp-blocks', 'wp-i18n', 'wp-element' ) );
+		}
 
-}
+	} // class
+
+}// if ( !class_exists )
