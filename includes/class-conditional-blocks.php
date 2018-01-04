5a6505a1bd399da50fe5b1ddb9d199ba6a0e5318
Shantanu <shantanu2846@gmail.com>
2018-01-04 13:12:10 +0530

    Define the  core plugin class

--- includes/class-conditional-blocks.php
+++ includes/class-conditional-blocks.php
@@ -1,1 +1,23 @@
 <?php
+
+/**
+ * Conditional Blocks
+ * 
+ * @author Shantanu Desai <shantanu2846@gmail.com>
+ * @since 0.0.1
+ */
+
+// If this file is called directly, abort.
+if ( !defined( 'ABSPATH' ) ) exit();
+
+if ( !class_exists( 'Conditional_Blocks' ) ) {
+
+	/**
+	 * Conditional Blocks
+	 *
+	 * @since 0.0.1
+	 */
+	class Conditional_Blocks {
+		
+	}
+}
