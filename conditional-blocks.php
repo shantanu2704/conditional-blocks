901e5aade08e7799c9b98bf8fdc15b3d8cf02f66
Shantanu <shantanu2846@gmail.com>
2018-01-04 12:57:34 +0530

    Update and rename the core plugin file with the plugin name

--- conditional-blocks.php
+++ conditional-blocks.php
@@ -9,37 +9,37 @@
  * a function that starts the plugin.
  *
  * @since             0.0.1
- * @package           plugin_slug
+ * @package	conditional-blocks
  *
  * @wordpress-plugin
  * 
- * Plugin Name: 
- * Description: 
- * Version: 
- * Author: 
- * Author URI:  
- * Text Domain: 
- * Domain Path: 
- * License: 
+ * Plugin Name: Conditional Blocks
+ * Description: Add conditional Gutenberg blocks
+ * Version: 0.0.1
+ * Author: Shantanu Desai
+ * Author URI:  www.github.com/shantanu2704
+ * Text Domain: conditional-blocks
+ * Domain Path: /languages
+ * License: GPL2	
  */
 
 // If this file is called directly, abort.
 if ( !defined( 'ABSPATH' ) ) exit();
 
-if ( !defined( 'PLUGIN_SLUG_PATH' ) ) {
+if ( !defined( 'CB_PATH' ) ) {
 	/**
 	 * Path to the plugin directory.
 	 *
 	 * @since 0.0.1
 	 */
-	define( 'PLUGIN_SLUG_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
+	define( 'CB_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
 }
 
-if ( !defined( 'PLUGIN_SLUG_URL' ) ) {
+if ( !defined( 'CB_URL' ) ) {
 	/**
 	 * URL to the plugin directory.
 	 *
 	 * @since 0.0.1
 	 */
-	define( 'PLUGIN_SLUG_URL', trailingslashit( plugin_dir_url(  __FILE__ ) ) );
+	define( 'CB_URL', trailingslashit( plugin_dir_url(  __FILE__ ) ) );
 }
